3126abedd5d54aaa032c2a82584542c8d45e7fd9
puja <pujarana1796@gmail.com>
2022-11-02 20:49:37 +0530

    [puja added]-regex validation for signin and signup page

--- book_store/src/pages/signin/signin.jsx
+++ book_store/src/pages/signin/signin.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signin.css'
 import Button from '@mui/material/Button';
 
 
 import TextField from '@mui/material/TextField';
 
+const emailRegex = /^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$/;
 function Signin() {
+    const [LoginObj, setLoginObj] = useState({ email: "", password: "" });
+    const [regexObj, setRegExObj] = useState({
+        emailBorder: false,
+        passwordBorder: false,
+        emailHelper: "",
+        passwordHelper: "",
+      });
+
+      const takeEmail = (event) => {
+        setLoginObj((prevState) => ({ ...prevState, email: event.target.value }));
+    };
+    const takePassword = (event) => {
+        setLoginObj((prevState) => ({ ...prevState, password: event.target.value }));
+    };
+
+    const OnSubmit = () => {
+        let emailTest = emailRegex.test(LoginObj.email);
+        let passwordTest = passwordRegex.test(LoginObj.password);
+    
+        if (emailTest === false) {
+          setRegExObj((prevState) => ({
+            ...prevState,
+            emailBorder: true,
+            emailHelper: "enter correct email",
+          }));
+        } else if (emailTest === true) {
+          setRegExObj((prevState) => ({
+            ...prevState,
+            emailBorder: false,
+            emailHelper: "",
+          }));
+        }
+        if (passwordTest === false) {
+          setRegExObj((prevState) => ({
+            ...prevState,
+            passwordBorder: true,
+            passwordHelper: "enter correct password",
+          }));
+        } else if (passwordTest === true) {
+          setRegExObj((prevState) => ({
+            ...prevState,
+            passwordBorder: false,
+            passwordHelper: "",
+          }));
+        }
+    
+        if (emailTest === true && passwordTest === true) {
+            console.log("hitt the server");
+        }
+      };
+
     return (
         <div>
             <div class="signinContainer">
@@ -23,11 +76,26 @@ function Signin() {
                         <div className="Data">
                             <div className="inputdetails">
                                 <label className="emailid">Email Id</label>
-                                <TextField id="outlined-basic" variant="outlined" size="small" placeholder=" Email Id " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", height: "50px" }} />
+                                <TextField id="outlined-basic" 
+                                                variant="outlined" 
+                                                size="small"
+                                                placeholder=" Email Id " 
+                                                style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", height: "50px" }}
+                                                onChange={takeEmail} 
+                                                error={regexObj.emailBorder} 
+                                                helperText={regexObj.emailHelper}/>
                                 <label className="emailid">Password</label>
-                                <TextField id="outlined-basic" variant="outlined" size="small" placeholder=" Password " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", height: "50px" }} />
+                                <TextField id="outlined-basic" 
+                                                variant="outlined" 
+                                                size="small" 
+                                                placeholder=" Password "
+                                                style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", height: "50px" }} 
+                                                onChange={takePassword}  
+                                                error={regexObj.passwordBorder} 
+                                                helperText={regexObj.passwordHelper}/>
                                 <label className="forgotpass" >Forgot Password?</label>
-                                <button >Login</button>
+                            
+                                <Button variant="contained" onClick={OnSubmit} >Login</Button>
                                 <text>OR</text>
                             </div>
                             <div className="FBGgl" >
